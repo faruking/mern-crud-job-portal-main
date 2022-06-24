de26e5cb8286576df503e747d57214035e8e46f3
faruking <faruk.abdulganiyu@gmail.com>
2022-06-24 13:32:00 +0100

    job  component uncommented

--- routes/job.js
+++ routes/job.js
@@ -1,31 +1,19 @@
-// const express = require("express");
+const express = require("express");
 
-// // jobRoutes is an instance of the express router.
-// // We use it to define our routes.
-// // The router will be added as a middleware and will take control of requests starting with path /job.
-// const jobRoutes = express.Router();
+// jobRoutes is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /job.
+const jobRoutes = express.Router();
 
-// // This will help us connect to the database
-// const dbo = require("../db/conn");
+// This will help us connect to the database
+const dbo = require("../db/conn");
 
-// // This help convert the id from string to ObjectId for the _id.
-// const ObjectId = require("mongodb").ObjectId;
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
 
-// var query = {location:"United Kingdom"};
-// // This section will help you get a list of all the jobs.
-// // jobRoutes.route("/job").get(function (req, res) {
-// //   let db_connect = dbo.getDb("reactdb");
-// //   db_connect
-// //     .collection("job")
-// //     .find({location:'United States'})
-// //     .toArray(function (err, result) {
-// //       if (err) throw err;
-// //       res.json(result);
-// //     });
-// // });
-
-
-// jobRoutes.route("/search").get(function (req, res) {
+var query = {location:"United Kingdom"};
+// This section will help you get a list of all the jobs.
+// jobRoutes.route("/job").get(function (req, res) {
 //   let db_connect = dbo.getDb("reactdb");
 //   db_connect
 //     .collection("job")
@@ -36,61 +24,73 @@
 //     });
 // });
 
-// // This section will help you get a single job by id
-// jobRoutes.route("/job/:id").get(function (req, res) {
-//   let db_connect = dbo.getDb();
-//   let myquery = { _id: ObjectId( req.params.id )};
-//   db_connect
-//       .collection("job")
-//       .findOne(myquery, function (err, result) {
-//         if (err) throw err;
-//         res.json(result);
-//       });
-// });
 
-// // This section will help you create a new job.
-// jobRoutes.route("/job/add").post(function (req, response) {
-//   let db_connect = dbo.getDb();
-//   let myobj = {
-//     name: req.body.name,
-//     position: req.body.position,
-//     level: req.body.level,
-//   };
-//   db_connect.collection("job").insertOne(myobj, function (err, res) {
-//     if (err) throw err;
-//     response.json(res);
-//   });
-// });
+jobRoutes.route("/search").get(function (req, res) {
+  let db_connect = dbo.getDb("reactdb");
+  db_connect
+    .collection("job")
+    .find({location:'United States'})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
 
-// // This section will help you update a job by id.
-// jobRoutes.route("/update/:id").post(function (req, response) {
-//   let db_connect = dbo.getDb();
-//   let myquery = { _id: ObjectId( req.params.id )};
-//   let newvalues = {
-//     $set: {
-//       name: req.body.name,
-//       position: req.body.position,
-//       level: req.body.level,
-//     },
-//   };
-//   db_connect
-//     .collection("job")
-//     .updateOne(myquery, newvalues, function (err, res) {
-//       if (err) throw err;
-//       console.log("1 document updated");
-//       response.json(res);
-//     });
-// });
+// This section will help you get a single job by id
+jobRoutes.route("/job/:id").get(function (req, res) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect
+      .collection("job")
+      .findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+      });
+});
 
-// // This section will help you delete a job
-// jobRoutes.route("/:id").delete((req, response) => {
-//   let db_connect = dbo.getDb();
-//   let myquery = { _id: ObjectId( req.params.id )};
-//   db_connect.collection("job").deleteOne(myquery, function (err, obj) {
-//     if (err) throw err;
-//     console.log("1 document deleted");
-//     response.json(obj);
-//   });
-// });
+// This section will help you create a new job.
+jobRoutes.route("/job/add").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myobj = {
+    name: req.body.name,
+    position: req.body.position,
+    level: req.body.level,
+  };
+  db_connect.collection("job").insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res);
+  });
+});
 
-// module.exports = jobRoutes;
+// This section will help you update a job by id.
+jobRoutes.route("/update/:id").post(function (req, response) {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  let newvalues = {
+    $set: {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level,
+    },
+  };
+  db_connect
+    .collection("job")
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+      response.json(res);
+    });
+});
+
+// This section will help you delete a job
+jobRoutes.route("/:id").delete((req, response) => {
+  let db_connect = dbo.getDb();
+  let myquery = { _id: ObjectId( req.params.id )};
+  db_connect.collection("job").deleteOne(myquery, function (err, obj) {
+    if (err) throw err;
+    console.log("1 document deleted");
+    response.json(obj);
+  });
+});
+
+module.exports = jobRoutes;
