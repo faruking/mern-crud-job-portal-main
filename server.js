1554c7db4eb3ec698b0a70920155667efe5b4dda
faruking <faruk.abdulganiyu@gmail.com>
2022-06-24 16:34:16 +0100

    job route added

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bodyParser = require("body-parser");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+app.use(require("./routes/job"));
 // get driver connection
 const dbo = require("./db/conn");
 // Accessing the path module
@@ -29,7 +30,6 @@ app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
-
   });
   console.log(`Server is running on port: ${port}`);
 });
