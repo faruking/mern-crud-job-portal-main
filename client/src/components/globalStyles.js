d0b3afe3396bd51538812f6c7296b075cde18574
faruking <faruk.abdulganiyu@gmail.com>
2022-06-24 14:16:28 +0100

    Revert "Revert "v""
    This reverts commit f3a07b29f6617f9f939600633c2adf1f52746c14.

--- client/src/components/globalStyles.js
+++ client/src/components/globalStyles.js
@@ -28,7 +28,7 @@ export const GlobalStyles = createGlobalStyle`
   .App-header{
       background: ${({theme}) => theme.bgBackground}
   }
-  .css-1tm81ay input{
+  .css-1tm81ay input,.css-1tm81ay{
     background-color: ${({theme}) => theme.contain}
   }
   .title:hover{
